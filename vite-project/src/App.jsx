3c05c8583a4a4efcb642f525107d7992477d308f
nameless <130059555+MeErickss@users.noreply.github.com>
2024-08-14 11:30:18 -0300

    header

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,21 +1,20 @@
+import Header from "./components/Header"
+
 function App() {
-  let nome = "Erick"
-
-  const soma = (a,b) => {
-    return a+b
-  }
-
-  const saudacao = (nome) => {
-    return <h2>Seja bem vindo {nome}</h2>
-  }
 
  return(
   <>
-    <h1>Oi {nome}</h1>
-    <p>{saudacao(nome)}</p>
-    <p>{soma(10,20)}</p>
+    {/* Header */}
+    <Header/>
+
+    {/* Main */}
+    <Main/>
+
+    {/* Footer */}
+    <Footer/>
+
   </>
- );
+ )
 }
 
 export default App

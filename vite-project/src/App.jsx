e946d7ff8030f9ac969c51aef2d6f1c899303409
nameless <130059555+MeErickss@users.noreply.github.com>
2024-08-23 11:50:31 -0300

    snippets

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,15 +1,18 @@
-import Header from './components/Header'
-import Main from './components/Main'
-import Footer from './components/Footer'
-
+import { Header } from "./components/Header/Header"
+import { Main } from "./components/Main/Main"
+import { Footer } from "./components/Footer/Footer"
 
 
 export default function App() {
   return (
     <div>
       <Header/>
-      <Main/>
-      <Footer/>
+      <Main>
+        <h3>Conteudo</h3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum nihil suscipit reprehenderit, odio, voluptates, voluptatem explicabo voluptatum a ad beatae blanditiis ducimus recusandae delectus iure! Vel itaque illum architecto ad.</p>
+      </Main>
+
+      <Footer autor="Nego"/>
     </div>
   )
 }
